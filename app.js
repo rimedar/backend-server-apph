446a007ec9411b1293004086a410439ab55b9071
Ricardo Ramirez <rimedar@hotmail.com>
2020-06-15 00:34:24 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 // Inicializar variables
 var app = express();
 
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var appUsuarios = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -16,16 +28,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 });
 
-
-// // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-
-});
+// Rutas
+app.use('/usuario', appUsuarios);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
